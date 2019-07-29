635d98556c6b032bd110bb958266e00e4baab74c
Goldstar <goldstar@tut.by>
2019-07-29 17:07:19 +0300

    MaxBuffer  = 4096 * 4;

--- include/pistache/config.h
+++ include/pistache/config.h
@@ -11,7 +11,7 @@ namespace Const
 {
     static constexpr size_t MaxBacklog = 128;
     static constexpr size_t MaxEvents  = 1024;
-    static constexpr size_t MaxBuffer  = 4096;
+    static constexpr size_t MaxBuffer  = 4096 * 4;
     static constexpr size_t DefaultWorkers = 1;
 
     static constexpr size_t DefaultTimerPoolSize = 128;
